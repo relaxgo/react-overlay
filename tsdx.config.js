71d3a3d5b891156f7882ba85eec8afedb2d8cc7c
Jiajun Zhang <jiajun@rodo.com>
2022-06-05 12:15:28 -0400

    chore: set up for first publish

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,8 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const copy = require('rollup-plugin-copy');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
@@ -13,9 +15,57 @@ module.exports = {
         ],
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
-        extract: options.writeMeta && 'react-overlay.css',
+        extract: options.writeMeta && 'style.css',
       })
     );
+
+    if (options.writeMeta) {
+      config.plugins.push(
+        copy({
+          targets: [
+            { src: 'LICENSE', dest: 'dist' },
+            { src: 'README.md', dest: 'dist' },
+            { src: 'package.json', dest: 'dist', transform: pickPackageFields },
+          ],
+        })
+      );
+    }
+
     return config;
   },
 };
+
+function pickPackageFields(contents, filename) {
+  const data = JSON.parse(contents);
+  const {
+    version,
+    license,
+    main,
+    typings,
+    files,
+    engines,
+    peerDependencies,
+    name,
+    author,
+    module,
+    dependencies,
+  } = data;
+
+  return JSON.stringify(
+    {
+      version,
+      name,
+      author,
+      license,
+      main,
+      module,
+      typings,
+      files,
+      engines,
+      peerDependencies,
+      dependencies,
+    },
+    null,
+    2
+  );
+}
